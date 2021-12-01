970eaf144a5c5c432f8dc4244463780073c3ce60
Aleksander Burmistrov <ab@piogroup.net>
2021-12-01 20:01:09 +0200

    add lesson 22

---
+++ 22/constant.php
@@ -0,0 +1,21 @@
+<?php
+
+//define('MY_CONST_NAME', 'asdfsdfsaf');
+//
+//echo MY_CONST_NAME;
+//
+//
+//
+//
+//class User {
+//
+//    const SEX_MALE = 'male';
+//
+//    const SEX_FEMALE = 'female';
+//
+//    public $name;
+//    public $sex;
+//}
+//
+//$alex = new User;
+//$alex->sex = User::SEX_MALE;
---
+++ 22/index.php
@@ -0,0 +1,46 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=season0921';
+$user = 'root';
+$pass = '123123';
+
+$connection = new PDO($dsn, $user, $pass);
+//$connection->exec('DELETE FROM user WHERE id = 29');
+//$connection->query('DELETE FROM user WHERE id = 28');
+//$connection->errorInfo();
+
+/* var PDOStatement */
+//$stm = $connection->query('SELECT name, age FROM user');
+//$stm->execute();
+//$list = $stm->fetch(PDO::FETCH_ASSOC);
+//
+
+$sql = 'DELETE FROM user WHERE id = :value and age = :age';
+
+$stm = $connection->prepare($sql);
+$stm->bindValue(':value', $_GET['id']);
+$stm->bindValue(':age', $_GET['age']);
+$stm->execute();
+
+$list = $stm->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+var_dump($list);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo 'OK';
