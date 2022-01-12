1120c8bdeb73da72445c48064838d5d312c53ff7
Aleksander Burmistrov <ab@piogroup.net>
2022-01-12 22:45:58 +0200

    add lesson 32

---
+++ 32/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// FTP - File Transfer Protocol
+// http://google.com
+// http://burm.drived.space
+// http://burm.me
+
+// ftp://burm.me
+// ftp://burm.drived.space
+//
+// login: burmme
+// passw: 4eG2cJ:zy32P(L
+// host: demo.burm.me
+
+// SSH - Secure SHell Protocol
+// SSH Port - 24560
+// Windows — Putty
+
+echo 'Hello from <strong>32</strong> lesson!';
